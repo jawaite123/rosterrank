0680c170ce01e0fdd12a404f8b9807e4812e80c1
Jeremy Waite <jawaite123@users.noreply.github.com>
2026-01-30 20:40:38 -0500

    update UI

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // IMPORTANT: Increment this version number every time you deploy changes
 // Example: 'rosterrank-v1', 'rosterrank-v2', 'rosterrank-v3', etc.
-const CACHE_NAME = 'rosterrank-v17';
+const CACHE_NAME = 'rosterrank-v18';
 const BASE_PATH = '/rosterrank';
 const urlsToCache = [
   BASE_PATH + '/',
